6af1ee53684e7a51a6b54de660daea6b3c824eea
Carlos Lopez <carlosmlopez066@gmail.com>
2023-01-18 10:28:48 -0400

    introduccion al estado en React

--- src/CreateTodoButtom.js
+++ src/CreateTodoButtom.js
@@ -2,8 +2,15 @@ import React from "react";
 import "./CreateTodoButtom.css"
 
 function CreateTodoButtom() {
+  const onClickBtn = (message) => {
+    alert(message)
+  }
   return (
-    <button className="CreateTodoButton">+</button>
+    <button
+      className="CreateTodoButton"
+      //onclick recibe una funcion
+      onClick={() => onClickBtn("aqui deberia haber un modal")}
+    >+</button>
   );
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./TodoItem.css"
 function TodoItem(props) {
+  const onComplete = () => {
+    alert("completaste la tarea" + props.text)
+  };
+  const onDelete = () => {
+    alert("borraste " + props.text)
+  };
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+      <span
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={onComplete}
+      >
         ✓
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">
+      <span
+        className="Icon Icon-delete"
+        onClick={onDelete}
+      >
         X
       </span>
     </li>
